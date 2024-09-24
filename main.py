946c7c5add5a27b96deba7a45a840a663ead3b28
rydercodes <jaber.rahimifard@outlook.com>
2024-09-24 17:29:57 +0200

    top chart added

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'config'
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'notebooks')))
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'data')))
 
-
 from google.cloud import storage
 from google.oauth2 import service_account
 
